322bbf3aafdb55a60c7c87efa417a938c8317d68
rooren_VM <ubuntu@ubuntu2004.linuxvmimages.local>
2023-11-06 01:01:24 -0500

    Add Chip8 state module

---
+++ Chip8State.cpp
@@ -0,0 +1,29 @@
+#include "Chip8State.h"
+
+// Constructor that takes a Chip8 instance and saves its state
+Chip8State::Chip8State(const Chip8& chip8) {
+    // Copy the state from the Chip8 instance to the Chip8State instance
+    memcpy(registers, chip8.registers, sizeof(registers));
+    memcpy(memory, chip8.memory, sizeof(memory));
+    index = chip8.index;
+    pc = chip8.pc;
+    memcpy(stack, chip8.stack, sizeof(stack));
+    sp = chip8.sp;
+    delayTimer = chip8.delayTimer;
+    soundTimer = chip8.soundTimer;
+    memcpy(keypad, chip8.keypad, sizeof(keypad));
+    memcpy(video, chip8.video, sizeof(video));
+    opcode = chip8.opcode;
+}
+
+// void Chip8State::SaveState(const char* filename, const Chip8State& state) {
+//     std::ofstream file(filename, std::ios::binary);
+//     file.write(reinterpret_cast<char*>(&state), sizeof(Chip8State));
+// }
+
+// void Chip8State::LoadState(const char* filename, Chip8State& state) {
+//     std::ifstream file(filename, std::ios::binary);
+//     if (file) {
+//         file.read(reinterpret_cast<char*>(&state), sizeof(Chip8State));
+//     }
+// }
---
+++ Chip8State.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "chip8.h"
+class Chip8;
+
+class Chip8State {
+public:
+  uint8_t registers[16]{};
+  uint8_t memory[4096]{};
+  uint16_t index{};
+  uint16_t pc{};
+  uint16_t stack[16]{};
+  uint8_t sp{};
+  uint8_t delayTimer{};
+  uint8_t soundTimer{};
+  uint8_t keypad[16]{};
+  uint32_t video[64 * 32]{};
+  uint16_t opcode;
+
+
+// Default constructor
+Chip8State() = default;
+// Constructor that takes a Chip8 instance and saves its state
+Chip8State(const Chip8& chip8);
+};
